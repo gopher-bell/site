c7e88f2b7ac80d1bc3cf340387c8637929ba3829
Jong <gopher.bell@gmail.com>
2023-04-17 18:44:32 +0900

    handler 생성
    fs, 일반 핸들러 생성

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,13 +5,15 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Jong!"))
-}
-
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
